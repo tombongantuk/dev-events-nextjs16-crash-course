4b4eff48c6820dcf3bbfd0996444b8b2a8ad4951
kristanto <kriselekyoband@gmail.com>
2026-06-28 23:10:51 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  reactCompiler:true,
+  experimental: {
+    turbopackFileSystemCacheForDev:true
+  }
 };
 
 export default nextConfig;
